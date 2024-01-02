cc7690046b22966b0a34a02f1a27b2dd336dfb02
Dennis Stine <dennis.stine@maritz.com>
2024-01-02 12:03:10 -0600

    Changed https service command

--- open_firewall.sh
+++ open_firewall.sh
@@ -1,10 +1,9 @@
 sudo firewall-cmd --get-active-zones
 
 sudo firewall-cmd --zone=docker --add-service=http --permanent
-sudo firewall-cmd --zone=docker --add-service=httpd --permanent
+sudo firewall-cmd --zone=docker --add-service=https --permanent
 sudo firewall-cmd --zone=docker --add-service=dns --permanent
 sudo firewall-cmd --zone=docker --add-service=dhcpv6-client --permanent
-sudo firewall-cmd --zone=docker --add-port=443/tcp --permanent
 
 sudo firewall-cmd --reload
 
