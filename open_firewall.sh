7ce90b26254301c9189a9e25617cd1c352205781
Dennis Stine <dennis.stine@maritz.com>
2024-01-02 12:04:38 -0600

    Switched to FedoraServer zone

--- open_firewall.sh
+++ open_firewall.sh
@@ -1,10 +1,10 @@
 sudo firewall-cmd --get-active-zones
 
-sudo firewall-cmd --zone=docker --add-service=http --permanent
-sudo firewall-cmd --zone=docker --add-service=https --permanent
-sudo firewall-cmd --zone=docker --add-service=dns --permanent
-sudo firewall-cmd --zone=docker --add-service=dhcpv6-client --permanent
+sudo firewall-cmd --zone=FedoraServer --add-service=http --permanent
+sudo firewall-cmd --zone=FedoraServer --add-service=https --permanent
+sudo firewall-cmd --zone=FedoraServer --add-service=dns --permanent
+sudo firewall-cmd --zone=FedoraServer --add-service=dhcpv6-client --permanent
 
 sudo firewall-cmd --reload
 
-sudo firewall-cmd --zone=docker --list-services
+sudo firewall-cmd --zone=FedoraServer --list-services
